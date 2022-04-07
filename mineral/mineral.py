6cad0aba070e36674551178554b1734faa76d754
sambitraze <majhisambit2@gmail.com>
2022-04-07 14:56:08 +0530

    DSad

--- mineral/mineral.py
+++ mineral/mineral.py
@@ -5,12 +5,11 @@ import os
 
 CONFIDENCE_THRESHOLD = 0.6
 NMS_THRESHOLD = 0.001
-item_list = ["blur", "covered", "empty", "iron", "redsand", "stone_and_whitesand", "tanker", "white_sand"]
 
 cap = cv2.VideoCapture('rtsp://admin:GT#6vK8D@192.168.150.105:554/cam/realmonitor?channel=1&subtype=0')
 prevTime = 0
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 #cv2.namedWindow("output", cv2.WINDOW_NORMAL)    
 
 class_names = []
@@ -18,9 +17,9 @@ with open("mineral.names", "r") as f:
     class_names = [cname.strip() for cname in f.readlines()]
 
     
-frame_width = int(cap.get(3))
-frame_height = int(cap.get(4))
-size = (frame_width, frame_height)
+# frame_width = int(cap.get(3))
+# frame_height = int(cap.get(4))
+# size = (frame_width, frame_height)
 
 
 colors = np.random.uniform(0,255,size=(len(class_names),3))
@@ -43,8 +42,8 @@ while cap.isOpened():
             if ctr%3==0:
                 if name%10000==0:
                     dir_n += 1
-                    if not os.path.exists("/home/pilot/External_Storage/anpr/new" + str(dir_n)):
-                        path = "/home/pilot/External_Storage/anprnew" + str(dir_n)
+                    if not os.path.exists("/home/pilot/External_Storage/mineral/new" + str(dir_n)):
+                        path = "/home/pilot/External_Storage/mineral/new" + str(dir_n)
                         os.mkdir(path)
                 f_name = "./new"+str(dir_n)+"/"+str(name)+".jpg"
                 cv2.imwrite(f_name, frame)
